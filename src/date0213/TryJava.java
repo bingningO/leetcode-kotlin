c2ce84f922a9095a5841539645f7856e2cd34204
bingningO <zhubingning@r.recruit.co.jp>
2019-02-18 14:50:07 +0900

    list

---
+++ src/date0213/TryJava.java
@@ -0,0 +1,9 @@
+package date0213;
+import java.util.*;
+import java.io.*;
+
+public class TryJava {
+    public static void main(String[] args) {
+        System.out.println("print test");
+    }
+}
