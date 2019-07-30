cda5669ce0c8331052bc17bdcdd50727d9e51495
bingningO <zhubingning@r.recruit.co.jp>
2019-07-30 16:52:11 +0900

    add improvement

---
+++ src/date0719/NumberOfIslands.kt
@@ -0,0 +1,29 @@
+package date0719
+
+import date0716.rob
+
+fun main(array: Array<String>) {
+//    println(rob(intArrayOf(1,2,3,1)))
+    println(rob(intArrayOf(2,7,9,3,1)))
+    println(rob(intArrayOf()))
+}
+
+fun numIslands(grid: Array<CharArray>): Int {
+    val xCount = grid[0].size
+    val yCount = grid.size
+    val countIsland = 1
+
+    for (indexY in 0 until yCount) {
+        for (indexX in 0 until xCount) {
+            if (grid[indexY][indexX] == '1') {
+                for (adjaceY in indexY + 1 until yCount) {
+                    for (adjaceX in indexX + 1 until xCount) {
+                        if (grid[adjaceY][adjaceX] == '0') {
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
